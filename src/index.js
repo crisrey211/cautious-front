0d238c599fe41991da1d0439ab3782b0da648df4
christian_reynaltt <creynaltt@maristak.net>
2024-05-04 00:38:28 +0100

    Feat: Mongoose added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+process.loadEnvFile()
 import app from './app.js'
 import { connectDB } from './db.js'
 
 connectDB()
-app.listen(3000)
-console.log('ABIERTO EN ', 3000)
+app.listen(process.env.PORT)
+console.log('ABIERTO EN ', process.env.PORT)
