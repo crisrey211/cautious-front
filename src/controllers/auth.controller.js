5147a62a4c6ec93285ef624660617f06a53954aa
christian_reynaltt <creynaltt@maristak.net>
2024-05-17 00:40:49 +0100

    Feat: Creación del token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,32 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js'
 
 export const register = async (req, res) => {
     const { email, password, username } = req.body
 
     try {
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User({
-            email,
-            password,
             username,
+            email,
+            password: passwordHash,
         })
         const userSaved = await newUser.save()
-        res.send(userSaved)
+
+        const token = createAccessToken({ id: userSaved._id })
+
+        res.cookie('token', token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        })
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ message: error.message })
     }
 }
 
