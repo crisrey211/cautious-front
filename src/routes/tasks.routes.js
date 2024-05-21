57851b9983b037444f4232c326bb288c581182cb
christian_reynaltt <creynaltt@maristak.net>
2024-05-21 21:50:33 +0100

    Feat: validacion de datos en el backend

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -7,12 +7,20 @@ import {
     updateTask,
     deleteTask,
 } from '../controllers/task.controller.js'
+import { createTaskSchema } from '../schemas/task.schema.js'
+import { validateSchema } from '../middlewares/validator.middleware.js'
+
 const router = Router()
 
 router.get('/tasks', authRequired, getTasks)
 
 router.get('/tasks/:id', authRequired, getTask)
-router.post('/tasks', authRequired, createTask)
+router.post(
+    '/tasks',
+    authRequired,
+    validateSchema(createTaskSchema),
+    createTask
+)
 router.delete('/tasks/:id', authRequired, deleteTask)
 router.put('/tasks/:id', authRequired, updateTask)
 
