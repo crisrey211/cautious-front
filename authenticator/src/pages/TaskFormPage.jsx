4ac251f71101647e73a5632ad99463fd555ad667
christian_reynaltt <creynaltt@maristak.net>
2024-07-29 22:55:46 +0200

    FixBug: date crashed. Now fixed

--- authenticator/src/pages/TaskFormPage.jsx
+++ authenticator/src/pages/TaskFormPage.jsx
@@ -18,6 +18,7 @@ const TaskFormPage = () => {
                 const task = await getTask(params.id)
                 setValue('title', task.title)
                 setValue('description', task.description)
+                setValue('date', dayjs(task.date).utc().format('YYYY-MM-DD'))
             }
             console.log(params)
         }
@@ -25,10 +26,15 @@ const TaskFormPage = () => {
     }, [])
 
     const onSubmit = handleSubmit((data) => {
+        const dataValid = {
+            ...data,
+            date: data.date ? dayjs.utc(data.date).format() : dayjs.utc().format(),
+        }
+
         if (params.id) {
-            updateTask(params.id, { date: dayjs.utc(data.date).format() })
+            updateTask(params.id, dataValid)
         } else {
-            createTask({ ...data, date: dayjs.utc(data.date).format() })
+            createTask(dataValid)
         }
         navigate('/tasks')
     })
